336a924eb98cf2b5f3dc3667bb89d640662b7b69
ITProfessor40148 <Zeus40148@hotmail.com>
2020-03-25 17:07:21 +0100

    fixed some issues

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="carousel-container">
-    <transition-group name="fade" tag="div">
-      <div v-for="i in [currentIndex]" :key="i">
-        <div>
-          <img :src="currentImg" class="item" />
-          <div class="title">
-            <h1>{{ titles[i] }}</h1>
-            <h2>{{ subTitles[i] }}</h2>
-            <p>{{ content[i] }}</p>
+    <b-container>
+      <b-row>
+        <transition-group name="fade" tag="div">
+          <div v-for="i in [currentIndex]" :key="i">
+            <div style="position: relative;">
+              <img :src="currentImg" class="item" height="475" />
+              <div class="title">
+                <p class="title--1">{{ titles[i] }}</p>
+                <h1 class="title--2">{{ subTitles[i] }}</h1>
+                <p class="title--3">{{ content[i] }}</p>
+                <a class="btn white_btn button_large">Button</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </transition-group>
-    <a class="prev" @click="prev" href="#">&#10094;</a>
-    <a class="next" @click="next" href="#">&#10095;</a>
+        </transition-group>
+        <a class="cprev" @click="prev" href="#">&#10094;</a>
+        <a class="cnext" @click="next" href="#">&#10095;</a>
 
-    <ol class="carousel-indicators">
-      <li
-        v-for="i in totalImg"
-        :key="i"
-        data-target="#banner_slider"
-        :data-slide-to="i"
-        :class="{ active: currentIndex === i - 1 }"
-        @click="setCustomIndex(i)"
-      ></li>
-    </ol>
+        <ol class="carousel-indicators1">
+          <li
+            v-for="i in totalImg"
+            :key="i"
+            data-target="#banner_slider"
+            :data-slide-to="i"
+            :class="{ active: currentIndex === i - 1 }"
+            @click="setCustomIndex(i)"
+          ></li>
+        </ol>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <script>
@@ -47,12 +52,12 @@ export default {
   },
 
   mounted: function() {
-    // this.startSlide();
+    this.startSlide();
   },
 
   methods: {
     startSlide: function() {
-      this.timer = setInterval(this.next, 4000);
+      this.timer = setInterval(this.next, 3000);
     },
 
     next: function() {
@@ -85,16 +90,64 @@ export default {
 </script>
 
 <style lang="css">
+.btn {
+  -webkit-border-radius: 3px;
+  border-radius: 3px;
+  padding: 10px 25px;
+  font-family: "Nunito", sans-serif;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 14px;
+  line-height: 19px;
+  letter-spacing: 0.03em;
+  border: 0;
+  position: relative;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.btn:not(:disabled):not(.disabled) {
+  cursor: pointer;
+}
+.btn.button_large {
+  height: 50px;
+  padding: 15px 25px;
+}
+.btn.white_btn {
+  background-color: #fafafb;
+  color: #283441;
+}
 .title {
   z-index: 999;
   position: absolute;
-  top: 150px;
-  left: 250px;
+  top: 50%;
+  left: 60px;
+  transform: translateY(-50%);
+}
+
+.title--1 {
+  background-color: pink;
+  height: 30px;
+  width: 80px;
+  border-radius: 5px;
+}
+
+.title--2,
+.title--3 {
+  color: white;
+}
+
+.title--3 {
+  text-align: left;
 }
 
 .carousel-container {
   position: relative;
-  height: 650px;
+  height: 475px;
 }
 
 .active {
@@ -105,13 +158,14 @@ export default {
 
 .item {
   border-radius: 50px;
+  height: 475px;
 }
 
-.carousel-indicators {
+.carousel-indicators1 {
   display: flex;
   right: auto;
-  bottom: 100px;
-  left: 60px;
+  bottom: 60px;
+  left: 20%;
   padding-left: 0;
   position: absolute;
   list-style: none;
@@ -120,7 +174,7 @@ export default {
   z-index: 15;
 }
 
-.carousel-indicators li {
+.carousel-indicators1 li {
   border-radius: 100%;
   position: relative;
   -ms-flex: 0 1 auto;
@@ -137,19 +191,37 @@ export default {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 0.9s ease;
+  animation: fadeIn ease 1.5s;
   overflow: hidden;
-  visibility: visible;
-  position: absolute;
+  position: relative;
   width: 100%;
   opacity: 1;
 }
 
 .fade-enter,
 .fade-leave-to {
-  visibility: hidden;
+  animation: fadeOut ease 1.5s;
+  display: none;
   width: 100%;
   opacity: 0;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 
 img {
@@ -157,12 +229,12 @@ img {
   width: 100%;
 }
 
-.prev,
-.next {
+.cprev,
+.cnext {
   cursor: pointer;
   position: absolute;
   top: 40%;
-  width: 58px;
+  width: 90px;
   padding: 16px;
   color: white;
   font-weight: bold;
@@ -173,16 +245,16 @@ img {
   user-select: none;
 }
 
-.next {
+.cnext {
   right: 10px;
 }
 
-.prev {
+.cprev {
   left: 10px;
 }
 
-.prev:hover,
-.next:hover {
+.cprev:hover,
+.cnext:hover {
   background-color: rgba(0, 0, 0, 0.9);
 }
 </style>
